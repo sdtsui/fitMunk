41eac9254745163314e1e55f951c4f88f20602ac
Devin Otway <devin.otway@gmail.com>
2015-02-04 14:37:31 -0800

    feat/adds controller logic

--- server/tournaments/controller.js
+++ server/tournaments/controller.js
@@ -1,21 +1,32 @@
 var db = require('../db.js');
+// var Q = require('q');
 
 var tournaments = {};
 
-tournaments.read = function(){
-
+tournaments.read = function(req, res, next){
+  // returns reference to tournaments in firebase
+  return db.child('tournaments').once('value', function(){
+    if (!data) {
+      console.err('[Error] No Tournaments Found');
+    } else {
+      res.send(data);
+    }
+  });
 };
 
-tournaments.create = function(){
-
+tournaments.create = function(req, res, next){
+  // returns reference to tournaments in firebase
+  return db.child('tournaments');
 };
 
-tournaments.update = function(tournament_id){
-
+tournaments.update = function(req, res, next){
+  // returns reference to a specific tournament in firebase
+  return db.child('tournaments').child(tournament_id);
 };
 
-tournaments.delete = function(tournament_id){
-
+tournaments.delete = function(req, res, next){
+  // returns reference to specific tournament in firebase
+  return db.child('tournaments').child(tournament_id);
 };
 
 
