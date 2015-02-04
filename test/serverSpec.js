29a7c56d80fae9864f0d4a6e7c8f707c28122029
sdtsui <danielt213@gmail.com>
2015-02-03 19:43:12 -0800

    added testing scaffold + dependencies for testing

---
+++ test/serverSpec.js
@@ -0,0 +1,31 @@
+var superagent    = require('superagent');
+var chai          = require('chai');
+var expect        = chai.expect;
+
+var exampleUsers = {
+  1: derpMonkey
+
+};
+
+var derpMonkey = {
+  id: 12345,
+  username: 'derpMonkey',
+  password: 'herpWord',
+} //
+
+var exampleTourney = {
+  t_id: 123,
+  name: 'worlds',
+  username: '!!',
+  password: '??',
+}
+
+describe('Testing CRUD functions at endpoints', function(){
+
+  describe('Create: ',function(){
+  });
+  describe('Remove: ',function(){});
+  describe('Update: ',function(){});
+  describe('Delete',function(){});
+
+})
