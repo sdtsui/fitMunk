97d801bdebf72c9e6ad56515b9ebd3ba287d9372
Daniel Tsui <danielt213@gmail.com>
2015-02-05 14:23:51 -0800

    Update tournamentListModel.js
    remove contents of file, for file removal later

--- server/users/tournamentListModel.js
+++ server/users/tournamentListModel.js
@@ -1,7 +1,1 @@
-var mongoose = require('mongoose');
 
-var tournamentsListSchema = new Mongoose.Schema({
-  tournament_id: String
-});
-
-module.exports = mongoose.model('tournamentsList', tournamentsListSchema);
