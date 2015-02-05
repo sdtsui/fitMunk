f7283245402544f63a726f013c879e95f0271dcc
Daniel Tsui <danielt213@gmail.com>
2015-02-05 14:23:18 -0800

    clear participantlist
    empty for delete, later

--- server/tournaments/participantListModel.js
+++ server/tournaments/participantListModel.js
@@ -1,7 +1,1 @@
-var mongoose = require('mongoose');
 
-var participantsListSchema = new mongoose.Schema({
-  user_id: String
-});
-
-module.exports = mongoose.model('participantsList', participantsListSchema);
