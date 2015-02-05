1c36fd000f1dce17ec85cd0dcbd799a87f8d6f10
sdtsui <danielt213@gmail.com>
2015-02-04 23:14:29 -0800

    wrote first test, tesing

--- test/serverSpec.js
+++ test/serverSpec.js
@@ -20,9 +20,22 @@ var exampleTourney = {
   password: '??',
 }
 
+var paths = {
+  apiTournaments: 'http://localhost:1337/api/tournaments/'
+}
+
+
 describe('Testing CRUD functions at endpoints', function(){
   console.log('sup');
-  describe('Create: ',function(){});
+  describe('Create: ',function(){
+    it('creates a new tournament', function(done){
+      superagent.post(paths.apiTournaments+'/12345')
+        .end(function(err, res){
+          console.log(res.statusCode);
+          done();
+        });
+    });
+  });
   describe('Remove: ',function(){});
   describe('Update: ',function(){});
   describe('Delete',function(){});
