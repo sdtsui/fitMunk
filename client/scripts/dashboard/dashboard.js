5c32181c36c7ee9bba0d6ef06e48c9e1a01f9b4c
chikeichan <chikeichan@localhost.(none)>
2015-02-04 17:27:42 -0800

    UI-Dashboard/Added overview widget

--- client/scripts/dashboard/dashboard.js
+++ client/scripts/dashboard/dashboard.js
@@ -14,3 +14,27 @@ angular.module('fm.dashboard', [])
 		}
 	}
 })
+
+.directive('fmInvites',function(){
+	return {
+		restrict  	 : 'EA',
+		scope				 : false,
+		replace   	 : true,
+		controller   : 'DashboardCtrl',
+		templateUrl  : '../scripts/dashboard/invites.html',
+		link				 : function(scope,el,attr){
+		}
+	}
+})
+
+.directive('fmOverview',function(){
+	return {
+		restrict  	 : 'EA',
+		scope				 : false,
+		replace   	 : true,
+		controller   : 'DashboardCtrl',
+		templateUrl  : '../scripts/dashboard/overview.html',
+		link				 : function(scope,el,attr){
+		}
+	}
+})
