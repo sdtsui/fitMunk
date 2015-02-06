0e4207d9e94c42eb8bfa692e6a8bdf0e0e35bddc
chikeichan <chikeichan@localhost.(none)>
2015-02-05 16:38:19 -0800

    Users/Integrate with new MongoDB API, removed/renamed firebase files, removed old front end files

--- server/users/controller.js
+++ server/users/controller.js
@@ -4,10 +4,12 @@ var User    = require('../users/model.js');
 var Tournaments = require('../tournaments/model.js');
 
 //Mongoose methods, promisified
-var findOneUser         = Q.nbind(User.find, User);
+var findOneUser         = Q.nbind(User.findOne, User);
 var findOneAndUpdate    = Q.nbind(User.findOneAndUpdate, User);
 var findOneAndRemove    = Q.nbind(User.findOneAndRemove, User);
 var findById            = Q.nbind(User.findById, User);
+var createUser          = Q.nbind(User.create, User);
+
 
 var controller = {};
 
@@ -27,8 +29,60 @@ controller.getTournaments = function(req, res, next) {
     });
 };
 
+controller.addUser = function (token, tokenSecret, profile, done){
+  var err = '';
+  findOneUser({user_id:profile.id})
+    .then(function(user){
+      if(!user){
+        var newUser = {};
+        newUser.avatar = profile._json.user.avatar;
+        newUser.user_id = profile.id;
+        newUser.tokenSecret = tokenSecret;
+        newUser.token = token;
+        newUser.full_name = profile._json.user.fullName;
+        newUser.strideRunning = profile._json.user.strideLengthRunning;
+        newUser.strideWalking = profile._json.user.strideLengthWalking;
+
+        createUser(newUser)
+          .then(function(d){
+            console.log('created user');
+            done(d);
+          })
+      } else {
+        console.log('Update User');
+          done(user);
+      }
+    })
+};
+
+controller.getUserStats = function (userID, callback) {
+  //take user id and query database
+  console.log(userID);
+  findOneUser({user_id:userID})
+    .then(function(user){
+      if(!user){
+        callback(new Error('no user'));
+      } else {
+        callback(user);
+      }
+    })
+};
+
+controller.addUserStats = function(userID, userStats) {
+  var stat = JSON.parse(userStats);
+  findOneUser({user_id:userID})
+    .then(function(user){
+      if(!user){
+        console.log('no user');
+      } else {
+        user.lifetimeSteps = stat.lifetime.total.steps;
+        user.save();
+      }
+    })
+};
+
 // Legacy path code
-module.exports = {
+var exports = {
   addUser: function (token, tokenSecret, profile, done){
     var err = '';
     //Add the user's profile info to the db
--- server/utils/fitbit.js
+++ server/utils/fitbit.js
@@ -11,14 +11,16 @@ module.exports = exports = {
       userAuthorizationURL: 'https://www.fitbit.com/oauth/authorize'
     }, function (token, tokenSecret, profile, done) {   
       //after oath login call this success handler
-          console.log(profile);
           //add user to db
-          Users.addUser(token, tokenSecret, profile);  
-          //this line waits for 26 to finish
-          exports.getStats(profile.id, token, tokenSecret).then(function() { 
-            //done tells the program you are done and want to go to the next step
-            done(null, profile._json.user); 
-          });
+          Users.addUser(token, tokenSecret, profile, function(d){
+            //this line waits for 26 to finish
+            exports.getStats(profile.id, token, tokenSecret).then(function() { 
+              //done tells the program you are done and want to go to the next step
+              console.log(d)
+              done(null, d); 
+            });
+          })
+
         }),
 
   getStats: function (userID, token, secret) {
@@ -26,7 +28,7 @@ module.exports = exports = {
     //creates the request to get activites json from fitbit
     return client.requestResource('/activities.json', 'GET', token, secret).then(function (data) {  
         //success handler for req, return the promise
-        Users.addUserStats(userID, data[0]); 
+        Users.addUserStats(userID,data[0]);
       }, function (err) {
         console.log('ERROR!',err);
       });
